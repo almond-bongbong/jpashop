fe6cf02ea462fad24143268df8e2ee525ecfd594
cmlee <bal.dongdongdong@gmail.com>
2019-10-16 22:20:26 +0900

    member logic

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,12 +1,12 @@
 package jpabook.jpashop.domain;
 
-import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter @Builder
+@Getter @Setter
 public class Address {
 
 	private String city;
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +16,7 @@ public class Member {
 	@Column(name = "member_id")
 	private Long id;
 
+	@Column(unique = true)
 	private String name;
 
 	@Embedded
